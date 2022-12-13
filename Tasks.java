ab040b0339fa342a30dfb0cda615879f55f0d224
reoicer <reoicer@outlook.com>
2022-12-13 13:40:53 +0800

    测试提交功能

---
+++ Tasks.java
@@ -0,0 +1,13 @@
+public class Tasks {
+//这是一个公共类，类名是Tasks，一个Java文件中只能有一个公共类，文件名必须与公共类名保持一致。
+    public static void main(String[] args){
+    //这是一句申明这是一个main方法，是整个程序的起点
+    //public是说明是公共方法，告诉其他的类可以访问这个函数
+    //static说明这是一个静态方法，不需要实例化即可使用
+    //void表明main()的返回值是无类型
+    //main代表这是主函数，是程序开始的地方
+    //String[] args是一个字符串数组，接收来自程序执行时传进来的参数
+        System.out.println("Hello World!");
+        //输出字符串语句，println的功能是输出并换行
+    }
+}
